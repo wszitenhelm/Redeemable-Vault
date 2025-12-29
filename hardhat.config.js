99ac3142b95becdc94bbcab518a7f39d765c1683
wszitenhelm <wiktoria.szitenhelm@gmail.com>
2025-12-29 18:17:17 +0100

    Modify hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");    // load the upgrades plugin
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
